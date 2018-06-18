6c1e2ac1039f8febec723d311b0274b28ad0e8ff
Ludwigm6 <Ludwigm6@students.uni-marburg.de>
2018-06-18 14:15:17 +0200

    create plot shapefiles

--- src/01c_alb_filter.R
+++ src/01c_alb_filter.R
@@ -24,7 +24,7 @@ saveRDS(alb_lui, "~/ma/data/alb/alb_lui.RDS")
 
 
 # # # # # # # # # # # 
-# get plot locations
+# get plot locations from RS db
 library(rPointDB)
 
 rs <- RemoteSensing$new("http://137.248.191.215:8081", "marvin.ludwig:XXX") # remote server
@@ -34,8 +34,35 @@ pois <- rs$poi_group("be_alb_poi")
 saveRDS(rois, "~/ma/data/plots/alb_rois.RDS")
 saveRDS(pois, "~/ma/data/plots/alb_pois.RDS")
 
+# # # convert grassland plots to shapefiles # # # # # 
+# ------------------------------------------------- #
+rois <- readRDS("~/ma/data/plots/alb_rois.RDS")
 
+rois_grass <- rois[substr(rois$name, 1, 3) == "AEG",]
+rois_points <- data.frame(EPID = rois_grass$name,
+                          x = unlist(lapply(rois_grass$center, "[[", 1)),
+                          y = unlist(lapply(rois_grass$center, "[[", 2)))
 
+coordinates(rois_points) <- ~ x + y
+projection(rois_points) <- crs("+init=epsg:32632")
+mapview(rois_points)
 
+writeOGR(rois_points, dsn = "/home/marvin/ma/data/plots/plot_points.shp",
+         driver = "ESRI Shapefile", layer = "plot_points")
+r_poly_coords <- rois_grass$polygon
+
+r_polygons <- lapply(seq(length(r_poly_coords)), function(p){
+  r_polygon <- Polygon(r_poly_coords[[p]])
+  r_polygons <- Polygons(list(r_polygon), p)
+  r_sp_polygons <- SpatialPolygons(list(r_polygons), proj4string = crs("+init=epsg:32632"))
+  return(r_sp_polygons)
+})
+rownames(rois_grass) <- seq(50)
+r_spdf <- SpatialPolygonsDataFrame(do.call(rbind, r_polygons), data = rois_grass)
+mapview(r_spdf)
+r_spdf@data$center <- NULL
+r_spdf@data$polygon <- NULL
+
+writeOGR(r_spdf, "/home/marvin/ma/data/plots/plot_polygons.shp", driver = "ESRI Shapefile", layer = "plot_polygons")
 
 
