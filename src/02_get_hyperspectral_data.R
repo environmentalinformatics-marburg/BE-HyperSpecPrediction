ac712f3f6fb1645698eea7d02a165e3c7b4ccc66
Marvin Ludwig <marvin.ludwig93@gmail.com>
2018-12-11 17:12:31 +0100

    include flexible environment + all exploritories

--- src/02_get_hyperspectral_data.R
+++ src/02_get_hyperspectral_data.R
@@ -1,44 +1,79 @@
-source("~/ma/BE-HyperSpecPrediction/src/00_set_environment.R")
+source("D:/Ludwig/be/BE-HyperSpecPrediction/src/00_set_environment.R", echo = FALSE)
 
-# get hyperspectral data for all plots
+# download 50x50m hyperspectral data for the available plots in every BE
+# after this script, define the centers for the corrected plot locations in QGIS
+# (without the climate station in the 10x10m plot)
 
-alb_hy <- rs$rasterdb("be_alb_hyperspectral")
+# # # Hainich--------------------
+#--------------------------------
+
+db_hy <- rs$rasterdb("be_hai_hyperspectral")
+
+rois <- rs$roi_group("be_hai_roi")
+rois <- rois[substr(rois$name, 1, 3) == "HEG",]
+
+# fix order
+rois <- rois[order(rois$name),]
+
+
+# get list of the extents of all plots
+re <- lapply(rois$polygon, extent)
+
+# get the 50 m plot data
+for(i in seq(length(re))){
+  hy <- db_hy$raster(re[[i]])
+  if(!is.na(hy[[1]][1,1])){
+    writeRaster(hy, paste0(p$hyperspectral$hainich$here, "hy_", names(re)[i], ".tif" )) 
+  }
+}
+
+
+# # # Schorfheide --------------------
+#-------------------------------------
+
+db_hy <- rs$rasterdb("be_sch_hyperspectral")
+
+rois <- rs$roi_group("be_sch_roi")
+rois <- rois[substr(rois$name, 1, 3) == "SEG",]
+
+# fix order
+rois <- rois[order(rois$name),]
+
+
+# get list of the extents of all plots
+re <- lapply(rois$polygon, extent)
+
+# get the 50 m plot data
+for(i in seq(length(re))){
+  hy <- db_hy$raster(re[[i]])
+  if(!is.na(hy[[1]][1,1])){
+    writeRaster(hy, paste0(p$hyperspectral$heide$here, "hy_", names(re)[i], ".tif" )) 
+  }
+}
+
+
+# # # Alb --------------------
+#-------------------------------------
+
+db_hy <- rs$rasterdb("be_alb_hyperspectral")
 
 rois <- rs$roi_group("be_alb_roi")
 rois <- rois[substr(rois$name, 1, 3) == "AEG",]
 
+# fix order
 rois <- rois[order(rois$name),]
-rownames(rois) <- seq(50)
+
+
 # get list of the extents of all plots
 re <- lapply(rois$polygon, extent)
 
-alb_hy_names <- names(re)
-
-# reduce plots to hyperspectral extend
-alb_hy$extent
-
 # get the 50 m plot data
 for(i in seq(length(re))){
-  hy <- alb_hy$raster(re[[i]])
-    if(!is.na(hy[[1]][1,1])){
-        writeRaster(hy, paste0(path$hyperspectral, "50m_plots_new/hy_", alb_hy_names[i], ".tif" ))  
-    }
+  hy <- db_hy$raster(re[[i]])
+  if(!is.na(hy[[1]][1,1])){
+    writeRaster(hy, paste0(p$hyperspectral$alb$here, "hy_", names(re)[i], ".tif" )) 
+  }
 }
-# HERE HAPPENS SOME QGIS MAGIC #
-# Visual observation of plots
-# Avoid climate station, trees, roads etc.
 
-# load corrected plot locations 
-plots <- readOGR("~/ma/data/plots/plots_corrected.shp")
-mapview(plots)
-
-pois <- data.frame(name = plots@data$EPID, x = plots@coords[,1], y = plots@coords[,2])
-pois <- pois[order(pois$name),]
-rownames(pois) <-seq(26)
-
-for(j in seq(nrow(pois))){
-  hy <- alb_hy$raster(extent_diameter(pois$x[j], pois$y[j], 10))
-  writeRaster(hy, paste0(path$hyperspectral, "10m_plots/hy_10m_", pois$name[j], ".tif" ))
-}
 
 
