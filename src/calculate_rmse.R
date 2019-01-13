32d0d676b626a8efa225a7f81d22658aa172f7f6
Ludwig, Marvin <marvin.ludwig@geo.uni-marburg.de>
2019-01-13 16:36:37 +0100

    RMSE for every EP, trait combination

---
+++ src/calculate_rmse.R
@@ -0,0 +1,47 @@
+# model validation based on all exploratories
+
+library(Metrics)
+library(reshape2)
+library(ggplot2)
+source("/home/hanna/repositories/envimaR/R/getEnvi.R")
+
+p <- getEnvi("/home/hanna/plant_traits/data/")
+
+
+res <- readRDS(paste0(p$results$here, "trait_prediction.RDS"))
+
+# include which exploratory it is
+res$EP <- substr(res$EPID, 1,1)
+res <- na.omit(res)
+
+
+# statements
+traits <- as.character(unique(res$target))
+ep <- c("A", "H", "S")
+
+statements <- expand.grid(traits, ep)
+i <- 1
+rmse_all <- lapply(seq(nrow(statements)), function(i){
+  t <- res$target == statements[i,1]
+  e <- res$EP == statements[i,2]
+  
+  all_folds <- lapply(seq(3,9), function(j){
+    data.frame(EP = statements[i,2],
+               fold = colnames(res)[j],
+               trait = statements[i,1],
+               RMSE = rmse(actual = res[t & e,]$observation, predicted = res[t & e,j]))
+   
+  })
+  do.call(rbind, all_folds)
+})
+
+rmse_df <- do.call(rbind, rmse_all)
+
+saveRDS(rmse_df, paste0(p$results$here, "ep_rmse.RDS"))
+
+
+
+
+
+
+
